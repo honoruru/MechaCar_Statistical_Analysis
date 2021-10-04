add20d86c939161902779da2e27094d754abf155
honoruru <psanchez@hawaiiusafcu.com>
2021-10-03 20:52:13 -1000

    Challenge Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,9 +4,35 @@ library(dplyr)
 
 MechaCar_df <- read.csv(file = 'MechaCar_mpg.csv',check.names = F, stringsAsFactors = F)
 
-lm(vehicle_length ~ mpg,MechaCar_df)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_df) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_df)) 
 
+
+
+lm(vehicle_length ~ mpg,MechaCar_df)
 summary(lm(vehicle_length ~ mpg,MechaCar_df))
+
+lm(vehicle_weight ~ mpg,MechaCar_df)
+summary(lm(vehicle_weight ~ mpg,MechaCar_df))
+
+lm(spoiler_angle ~ mpg,MechaCar_df)
+summary(lm(spoiler_angle ~ mpg,MechaCar_df))
+
+lm(ground_clearance ~ mpg,MechaCar_df)
+summary(lm(ground_clearance ~ mpg,MechaCar_df))
+
+lm(AWD ~ mpg,MechaCar_df)
+summary(lm(AWD ~ mpg,MechaCar_df))
+
+lm(mpg ~ mpg,MechaCar_df)
+summary(lm(mpg ~ mpg,MechaCar_df))
+
+
+cor(MechaCar_df$vehicle_length, MechaCar_df$mpg)
+cor(MechaCar_df$vehicle_weight, MechaCar_df$mpg)
+cor(MechaCar_df$spoiler_angle, MechaCar_df$mpg)
+cor(MechaCar_df$ground_clearance, MechaCar_df$mpg)
+cor(MechaCar_df$AWD, MechaCar_df$mpg)
 
 
 # DELIVERABLE 2
@@ -30,4 +56,4 @@ Lot_3 <- subset(MechaCar_tbl, Manufacturing_Lot = "Lot3")
 
 t.test(Lot_1$PSI, mu=1500)
 t.test(Lot_2$PSI, mu=1500)
-t.test(Lot_2$PSI, mu=1500)
+t.test(Lot_3$PSI, mu=1500)
